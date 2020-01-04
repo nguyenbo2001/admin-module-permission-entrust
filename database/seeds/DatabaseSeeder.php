07e6089ba2e6993c365d5fdacadd8fdb18c96f8e
Bo Nguyen <nguyenbo2001@gmail.com>
2020-01-04 20:05:26 +0700

    add database

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -1,5 +1,8 @@
 <?php
 
+use App\User;
+use App\Role;
+use App\Permission;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -12,5 +15,32 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // $this->call(UsersTableSeeder::class);
+        DB::table('users')->delete();
+        DB::table('roles')->delete();
+
+        // craete admin role
+        $role = [
+            'name' => 'admin',
+            'display_name' => 'Admin',
+            'description' => 'Full Permission',
+        ];
+        $role = Role::create($role);
+
+        // set role permission
+        $permissions = Permission::all();
+        foreach ($permissions as $key => $permission) {
+            $role->attachPermission($permission);
+        }
+
+        // create admin user
+        $user = [
+            'name' => 'Admin User',
+            'email' => 'nguyenbo2001@gmail.com',
+            'password' => bcrypt('abc123456'),
+        ];
+        $user = User::create($user);
+
+        // set user role
+        $user->attachRole($role);
     }
 }
